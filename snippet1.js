bbc68a9da58d2531cf0378d66892000cfe6167eb
snehal.patra <snehal.patra@emids.com>
2023-03-16 11:39:51 +0530

    Added initial code snippets on Javascript strings

---
+++ snippet1.js
@@ -0,0 +1,7 @@
+let x = "John";
+let y = new String("John");
+const a = x==y ? true : false;
+const b = x===y ? true : false;
+console.log("a="+ a + " b="+b);
+console.log("Type of x="+typeof(x));
+console.log("Type of y="+typeof(y));
