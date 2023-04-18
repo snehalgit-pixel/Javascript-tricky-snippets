67b29f59d6ea2b35e0a963e652c9a95304ec5b7c
snehal.patra <snehal.patra@emids.com>
2023-04-18 10:35:54 +0530

    More tricky snippets involving this & IIFE

---
+++ snippet32.js
@@ -0,0 +1,10 @@
+(
+    function() {
+        var a = b = 3;
+    }
+)();
+console.log("a defined? " + (typeof a !== 'undefined'));
+console.log("b defined? " + (typeof b !== 'undefined'));
+
+// b getting global scope
+// a getting local scope only within the function
